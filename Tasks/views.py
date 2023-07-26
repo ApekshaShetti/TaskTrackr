f58594b8f1f6a6b0dbd574978218a599e9cec65c
Apeksha <apekshashetti1012@gmail.com>
2023-07-26 21:29:10 +0530

    crud view complete

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
+from django.urls import reverse_lazy
 
 
 class TaskList(ListView):
     model = Task 
+    context_object_name = 'task_list'
+
+class TaskDetail(DetailView):
+    model = Task 
+    context_object_name = 'task'
+    template_name = 'Tasks/task.html'
     
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task_list')  # redirect to task list page
+
+
+class UpdateTask(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task_list')  # redirect to task list page
+
+class DeleteTask(DeleteView):
+    model = Task
+    context_object_name = 'task_delete'
+    success_url = reverse_lazy('task_list')  # redirect to task list page
+
+
+
 
 
 
