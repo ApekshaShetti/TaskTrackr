99573276f1c33f4d965f79c49bb5799811b5cf49
Apeksha <apekshashetti1012@gmail.com>
2023-07-26 18:09:53 +0530

    setup complete

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,11 +1,21 @@
 from django.db import models
-import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True,blank=True)
+    # models.cascade is for if user gets deleted all data should be deleted of that user
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True,blank=True)
     status = models.BooleanField(default=False)
-    date = models.DateField(default=datetime.datetime.today)
-    category = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    # category = models.CharField(max_length=50)
+
+    # default value of model
+    def __str__(self):
+        return self.title
+    
+    # default ordering whose status is completed all those will be at bottom
+    class Meta:
+        ordering = ['status']
 
