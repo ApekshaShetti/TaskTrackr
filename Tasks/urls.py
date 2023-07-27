ca106e1fd325b8e86aeeff2244f1484d3eb2c22d
Apeksha <apekshashetti1012@gmail.com>
2023-07-27 19:29:35 +0530

    registe and search complete

--- Tasks/urls.py
+++ Tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, CreateTask, UpdateTask, DeleteTask, Login
+from .views import TaskList, TaskDetail, CreateTask, UpdateTask, DeleteTask, Login, Register
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/',Login.as_view(), name='login'),
     path('logout/',LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/',Register.as_view(), name='register'),
     path('',TaskList.as_view(), name='task_list'),
     path('task/<int:pk>/',TaskDetail.as_view(), name='task'),  # dynamic routing
     path('create-task/',CreateTask.as_view(), name='create-task'),
